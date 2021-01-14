3a4f481bf86612bb2d8da244ef28692f3f14d33d
Shannon L Johnson-Martin <shannonljohnson-martin@Shannons-MacBook-Pro.local>
2021-01-14 18:53:20 -0500

    Add app.use public

--- server.js
+++ server.js
@@ -5,18 +5,18 @@ var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(express.static("public"));
 
 // routes
-var htmlRoutes = require("./routes/htmlRoutes");
-var apiRoutes = require("./routes/apiRoutes");
-app.use(htmlRoutes);
-app.use(apiRoutes);
+// var htmlRoutes = require("./routes/htmlRoutes");
+// var apiRoutes = require("./routes/apiRoutes");
+// app.use(htmlRoutes);
+// app.use(apiRoutes);
 
 // require("./routes/apiRoutes")(app);
-// require("./routes/htmlRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 
 app.listen(PORT, function(){
-    console.log("App listening on port " +PORT);
+    console.log("App listening on port http://localhost:" +PORT);
 });
