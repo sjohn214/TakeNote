bfe8d73738c8f991e48007d5021db62060914aca
Shannon L Johnson-Martin <shannonljohnson-martin@Shannons-MacBook-Pro.local>
2021-01-13 20:55:52 -0500

    Add require express, routes, port

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// routes
+var htmlRoutes = require("./routes/htmlRoutes");
+var apiRoutes = require("./routes/apiRoutes");
+app.use(htmlRoutes);
+app.use(apiRoutes);
+
+// require("./routes/apiRoutes")(app);
+// require("./routes/htmlRoutes")(app);
+
+
+app.listen(PORT, function(){
+    console.log("App listening on port " +PORT);
+});
