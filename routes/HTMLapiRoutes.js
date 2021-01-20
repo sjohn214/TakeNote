696b7d10cba97c7b48af6f2948d433ee07978165
Shannon L Johnson-Martin <shannonljohnson-martin@Shannons-MBP.attlocal.net>
2021-01-19 20:11:38 -0500

    Add file clean up

--- routes/HTMLapiRoutes.js
+++ routes/HTMLapiRoutes.js
@@ -1,49 +1,14 @@
 var path = require("path");
 var fs = require("fs");
 var router = require("express").Router();
-// var util = require("util");
 var uuid = require("uuid/v1");
 
-// var readFileAsync = util.promisify(fs.readFile);
-// var writeFileAsync = util.promisify(fs.writeFile);
 
 fs.readFile("db/db.json","utf8", (err, data) => {
     if (err) throw err;
     console.log(data);
 });
 var notes = require("../db/db.json");
-
-// class savedData {
-//     read() {
-//         return readFileAsync("db/db.json", "utf8");
-//     }
-//     write(note) {
-//         return writeFileAsync("db/db.json", JSON.stringify(note));
-//     }
-//     getNotes() {
-//         return this.read().then(function (notes) {
-//             var parsedNotes
-//             try {
-//                 parsedNotes = [].concat(JSON.parse(notes));
-//             } catch (err) {
-//                 parsedNotes = [];
-//             }
-//             return parsedNotes;
-//         })
-//     }
-//     addNote(note) {
-//         var {title, text} = note;
-//         var newNote = {title, text, id: uuid()};
-//         return this.getNotes().then(function(notes) {
-//             return[...notes, newNote]
-//         }).then((updatedNotes)=> { 
-//             console.log(updatedNotes);
-//            this.write(updatedNotes);
-//         }).then(function() {
-//             newNote;
-//         })
-//     }
-// }
 
 
 // HTML route set-up//
@@ -76,11 +41,6 @@ router.post("/api/notes", function (req, res) {
         res.json(notes);
     });
     //will set-up api post route and receive any newly added note. Adds as db.json and returns the added note.//
-
-// router.get("/api/notes/:id", function (req, res){
-//     res.json(notes[req.params.id]);
-// });
-// // will allow for retreiving of note with specific id.//
 
 router.delete("/api/notes/:id", function(req, res) {
     notes.splice(req.params.id, 1);
